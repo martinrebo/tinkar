484934dd459ba02ec7ecc91fe01e96c2afc16158
martinrebo <martinrg33@gmail.com>
2019-05-12 16:59:29 +0200

    0.6.6 Remove Social Media buttons from Login

--- src/Pages/Home/LoginPage.js
+++ src/Pages/Home/LoginPage.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import UserMenu from '../../Components/UserMenu/UserMenu';
-import { Icon, Card, Container, Label, } from 'semantic-ui-react';
+import { Card, Container,} from 'semantic-ui-react';
 
 export default function LoginPage() {
     return (
@@ -9,15 +9,6 @@ export default function LoginPage() {
                 <Card.Content> <Card.Header>  ESCard </Card.Header> </Card.Content>
                 <Card.Content> 
                     <Card.Description> Tinder style Javascript cards </Card.Description> </Card.Content>
-                <Card.Content>
-                    <Card.Description> Follow us:   </Card.Description>
-                    <br></br>
-                    <Label>
-                        <a href="https://github.com/martinrebo/escard" target="_blank" rel="noopener noreferrer">
-                            <Icon link size="large" color="teal" name="github" /> Github </a> </Label>
-                    <Label>
-                        <a href="https://twitter.com/warteamx"> <Icon link size="large" color="teal" name="twitter"></Icon> Twitter @warteamx </a> </Label>
-                </Card.Content>
                 <Card.Content>
                     <UserMenu />
                 </Card.Content>
